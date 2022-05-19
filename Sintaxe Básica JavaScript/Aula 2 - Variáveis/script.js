a739f171db4c5a309a5f506d76d9a656ac58afc3
Itielpedro <itielpedro2014@gmail.com>
2022-05-19 13:52:16 -0300

    Add Sintaxe Básica JS

---
+++ Sintaxe Básica JavaScript/Aula 2 - Variáveis/script.js
@@ -0,0 +1,114 @@
+// Tipos Primitivos
+/*
+// boolean
+var vOuF = true;
+console.log(vOuF);
+console.log(typeof(vOuF));
+
+// number
+var num = 16;
+console.log(num);
+console.log(typeof(num));
+
+// string
+var nome = "Pedro"
+console.log(nome);
+console.log(typeof(nome));
+
+// como declarar
+var variavel = "João";
+console.log(variavel);
+variavel = 5;
+console.log(variavel);
+
+let  n1 = "Maria";
+console.log(n1);
+n1 = 10;
+console.log(n1);
+
+// Valor não alterável, o que causa um erro no console
+const  n2 = "Maria";
+console.log(n2);
+n2 = "José";
+console.log(n2);
+
+// Variáveis com escopo global e local
+var global = "global";
+console.log(global)
+
+// Variáveis com escopo e local
+function escopoLocal(){
+    let local = "local";
+}
+
+//Retorna um erro por ser uma variável local
+console.log(local);
+
+// Atrubuição
+var atribuicao = "Lucas";
+
+// Comparação
+var comparacao = "0" == 0;
+console.log(comparacao)
+
+// Comparação Identica
+var comparacaoIdentica = "0" === 0;
+console.log(comparacaoIdentica)
+
+//Operadores Aritiméticos
+
+// Adição
+var soma = 10 + 5;
+console.log(soma);
+
+// Subtração
+var sub = 10 - 5;
+console.log(sub,);
+
+// Multiplicação
+var mult = 10 * 5;
+console.log(mult);
+
+// Divisão
+var div = 10 / 5;
+console.log(div);
+
+// Módulo  da Divisão ou Divisão Inteira
+var divmod = 5 % 2;
+console.log(divmod);
+
+// Potenciação
+var pot = 10 ** 2;
+console.log(pot);
+
+//Operadores Relacionais
+
+// Maior Que
+var maior = 5 > 2;
+console.log(maior)
+
+// Menor que
+var menor = 5 < 2;
+console.log(menor)
+
+// Maior ou Igual a
+var maiorIgual = 5 >= 5;
+console.log(maiorIgual)
+
+// Menor ou Igual a
+var menorIgual = 5 <= 10;
+console.log(menorIgual)*/
+
+//Operadores Lógicos
+
+// E(&&)
+var e = true && false;
+console.log(e)
+
+// OU(||)
+var ou = true || false;
+console.log(ou)
+
+// NÃO(||)
+var nao = !false;
+console.log(nao)
