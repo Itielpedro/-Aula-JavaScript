2da937f4e877390d6541a836ec55baae48c9e530
Itielpedro <itielpedro2014@gmail.com>
2022-05-20 12:35:53 -0300

    Add Aulas 4,5 e 6

---
+++ Sintaxe Básica JavaScript/Aula 5 - Funções/script.js
@@ -0,0 +1,14 @@
+// Funções em JavaScript
+
+// Funçao sem parâmetros
+function funcao(){
+    console.log("Esta é uma função sem parâmetros!")
+}
+funcao()
+
+// Funçao com parâmetros
+function parametros(mensagem1, mensagem2){
+    console.log(mensagem1, mensagem2)
+}
+
+parametros("Esta mensagem vem de uma função", "com dois parâmetros")
